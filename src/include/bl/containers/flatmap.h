129e6b0a24e9d6ce6a9dd9a17dbf7635d8d0d946
Brinq q <brinqq>
2025-08-29 23:56:06 -0700

    started flathashmap

--- src/include/bl/containers/flatmap.h
+++ src/include/bl/containers/flatmap.h
@@ -3,97 +3,213 @@
 #include "../bldefines.h"
 #include "functional"
 
+
+// Not thread safe.
+
 namespace bl{
 
-template<class _Key, class _Value>
-class FlatMap{
+template<class _K, class _V, class _Allocator = std::allocator<bl::byte>>
+class FlatMap : private _Allocator {
 private:
 
-std::hash<_Key> hasher;
-
 struct Node{
-  bool freeFlag = true;
-  _Value value;
+  _V value;
+  bool isOccupied = false;
+};
+
+struct EndSentinal{
+  uint8_t end = 0xFF;
 };
 
 struct iPair{
-  _Key key;
-  _Value& value;
+  _K key;
+  _V& value;
 };
 
-Node* m_pMem;
-uint32_t m_elements;
+std::hash<_K> hasher;
+Node* pMem;
+uint32_t size;
+size_t maxSize;
 
 public:
-  _Value* begin();
-  _Value* end();
 
+//__________________________________________________________________________
+// TODO: Add sentinel at end of buffer so iterator can drop the extra end pointer and fix the double loop on begin and end.
+// just fix all this.
+class iterator{
 private:
-inline size_t Hash(const _Key& key){
-  return hasher(key) % 5;
-} 
-
-//@note returns -1 if no avaliable index is found.
-inline int32_t ProbeForIndex(Node* ptr){
-  Node* pEnd = m_pMem + m_elements;
-  Node* pTop = ptr;
-  Node* pBottom = ptr;
-
-  while(pTop != pEnd){
-    if(pTop->freeFlag == true){
-      return pTop - m_pMem;
+  Node* itr;
+  Node* end;
+public:
+  iterator(Node* mem, Node* end):itr(mem), end(end){
+    while(itr->isOccupied == false && itr != end){
+      itr++;
     }
-    pTop++;
+
+    while(this->end->isOccupied == false && this->end != itr){
+      this->end--;
+    }
+  }
+  iterator(Node* mem):itr(mem){ }
+
+  _V& operator*(){return itr->value;}
+  bool operator!=(bl::FlatMap<_K, _V>::iterator& other){ return itr != other.itr;}
+
+  iterator& operator++(){
+    itr++;
+    while(itr->isOccupied == false && itr != end){
+      itr++;
+    }
+    return *this;
   }
 
-  while(pBottom != m_pMem){
-    if(pBottom->freeFlag == true){
-      printf("iiiiii: %i\n", m_pMem - pBottom);
-      return pBottom - m_pMem;
+  // iterator& operator++(int){
+  //   itr++;
+  //   while(itr->freeFlag == true && itr != end){
+  //     itr++;
+  //   }
+  //   return *this;
+  // }
+};
+
+  inline iterator begin(){ 
+    return iterator(pMem, (pMem + maxSize) + 1);
+  };
+  inline iterator end(){return iterator(pMem, (pMem + maxSize) + 1);};
+//__________________________________________________________________________
+
+private:
+inline size_t Hash(const _K& key){
+  return hasher(key) % (maxSize);
+} 
+
+
+//@note returns -1 if no avaliable index is found.
+inline int32_t ProbeForIndex(Node* buf, Node* end, Node* ptr){
+  Node* itr = ptr;
+
+  //when we prob from the beginning of the buffer since we use a while loop 
+  //it will never find index 0 causing us to return no index every time we need a 0 index
+  //so we add this check.
+  //TODO: remove this check so we dont have branching here.
+  if(itr == buf){
+    if(itr->isOccupied == false ){
+      return itr - buf;
     }
-    pBottom--;
+  }
+
+  while(itr != end){
+    if(itr->isOccupied == false){
+      return itr - buf;
+    }
+    itr++;
+  }
+
+
+  itr = buf;
+  while(itr != ptr){
+    if(itr->isOccupied == false){
+      return itr - buf;
+    }
+    itr++;
   }
 
   return -1;
 };
 
-inline void Alloc(const uint64_t bytes){
-  m_pMem = new Node[bytes / sizeof(Node)];
+inline void* Alloc(const uint32_t size){
+  const EndSentinal sentinel{};
+  this->maxSize = size;
+  _Allocator& allocator = static_cast<_Allocator&>(*this);
+  Node* buf = (Node*)allocator.allocate((sizeof(Node) * maxSize) + sizeof(EndSentinal));
+  memset(buf, 0, sizeof(Node) * maxSize);
+  *((EndSentinal*)buf + (maxSize)) = sentinel;
+  return buf;
 }
 
-
-inline void DeAlloc(){
-  delete[] m_pMem;
+inline void Dealloc(void* ptr){
+  _Allocator& allocator = static_cast<_Allocator&>(*this);
+  allocator.deallocate((bl::byte*)ptr, maxSize * sizeof(Node));
 }
+
+inline void Realloc(const uint32_t size){
+  // Node* newMem = (Node*)Alloc(size);
+  // Dealloc(pMem);
+  // pMem = newMem;
+};
+
 
 public:
 inline FlatMap(){}
 inline ~FlatMap(){}
 
+//clears the entire buffer
+//
+// @param
+inline void clear(){}
+
 // Inserts a new node.
 //
-// @param[in] key - Key used to hash.
-// @param[in] value - value to be stored. 
-inline void Insert(_Key key, _Value& val){
+// @param [in] key& - Key used to hash.
+// @param [in] value& - value to be stored. 
+inline void Insert(_K& key, _V& val){
+    if(pMem == nullptr){pMem = (Node*)Alloc(10);};
+    
+    size++;
     size_t index = Hash(key);
-    Node* ptr = m_pMem + index;
+    Node* ptr = pMem + index;
+    printf("Hash evaluated to %zu\n", index);
 
-    if(ptr->freeFlag == false){
-      // TODO: find next open slot
-      printf("Collision at index: %zu for key: %i\n", index, key);
-      index = ProbeForIndex(ptr);
+    if(ptr->isOccupied == true){
+      printf("Collosion at index: %zu\n", index, key);
+      index = ProbeForIndex(pMem, (pMem + size) + 1, pMem + index);
       if(index == -1){
-        printf("Failed to find index\n");
+        printf("Reallocating\n");
+        Realloc(size * 2);
+        printf("-------------------------------------------------------------------\n");
         return;
       }
+      printf("Collosion resolved new index:%zu\n", index);
 
-      printf("Found Index at %zu\n", index);
-      ptr = m_pMem + index;
+      ptr = pMem + index;
     }
 
-    printf("Inserting Value: %i at index: %zu. No Collision for key: %i\n", val, index, key);
-    ptr->freeFlag = false;
+  
+    printf("inserting %i at index %zu - orginal hash %i\n", val, index, key);
+    printf("-------------------------------------------------------------------\n");
+    ptr->isOccupied = true;
     ptr->value = val;
+}
+
+// Takes in a copy of _Value and inserts it into a new node.
+//
+// @param [in] key - Key used to hash.
+// @param [in] value - value to be stored. 
+inline void InsertCopy(_K key, _V val){
+    if(pMem == nullptr){pMem = (Node*)Alloc(3);};
+    
+    size_t index = Hash(key);
+    Node* ptr = pMem + index;
+
+    if(ptr->isOccupied == true){
+      // TODO: find next open slot
+      index = ProbeForIndex(ptr);
+      if(index == -1){
+        return;
+        Realloc();
+        probForIndex(ptr);
+      }
+
+      ptr = pMem + index;
+    }
+
+    ptr->isOccupied = true;
+    ptr->value = val;
+}
+
+// Constructs object and inserts into map into map.
+inline void FlatMap::InsertEmplace(){
+
 }
 
 
@@ -101,8 +217,8 @@ inline void Insert(_Key key, _Value& val){
 //
 // @param[in] std::inializer_list<class, class>.
 inline FlatMap(std::initializer_list<iPair> list){
-  m_elements = list.size();
-  Alloc(m_elements * sizeof(Node));
+  size = list.size();
+  Alloc(size * sizeof(Node));
 
   for(iPair e : list){
     Insert(e.key, e.value);
@@ -115,9 +231,25 @@ inline FlatMap(std::initializer_list<iPair> list){
 //@param[in] size - how many elements the memory buffer will be able to hold.
 //@note invalidates all iterators, and copies the current buffer data into the new allocated memory buffer.
 inline void Reserve(const uint32_t size){
-  //tmp
-  m_elements = size;
-  m_pMem = new Node[size];
+  Node* pNewMemory = Alloc(size);
+
+  Node* pEnd = pMem + maxSize;
+  Node* itr = pMem;
+
+  uint32_t offset = 0;
+  while(itr != pEnd){
+    if(itr->isOccupied == true){
+       pNewMemory[offset].isOccupied = true;
+       pNewMemory[offset].val = pMem[offset].val;
+    }
+
+    offset++;
+    itr++;
+  }
+
+  deconstruct(pMem);
+  pMem = pNewMemory;
+  maxSize = (size * sizeof(Node));
 }
 
 };
