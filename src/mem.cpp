78bc5f2d43cd14ca3ee32b1d15451153c2625413
brinq <andrewrdavis18@gmail.com>
2025-12-12 15:09:58 -0800

    Moved alignment utils into memory header.

--- src/mem.cpp
+++ src/mem.cpp
@@ -1,6 +1,1 @@
 #include "bcl/memory/memory.h"
-
-namespace bcl{
-  // _BCLCONSTEXPR11 size_t align_p2(const size_t size, const size_t align){return (size + align - 1) & ~(align - 1);}
-  // _BCLCONSTEXPR11 size_t aligned_size(const size_t size, const size_t align){ return (size + align) - (size % align);}
-}
