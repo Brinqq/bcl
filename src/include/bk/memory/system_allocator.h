0e72bd0e48d89ea5ccaa60938150a657328f8e09
brinq <andrewrdavis18@gmail.com>
2026-07-24 01:30:05 -0700

    system allocator

---
+++ src/include/bk/memory/system_allocator.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "bk/defines.h"
+#include "bk/features.h"
+
+class SystemAllocator{
+public:
+  void* alloc(size_t n_bytes, size_t align){
+    return malloc(n_bytes);
+  };
+
+  void dealloc(void* mem){
+    free(mem);
+  }
+
+  void dealloc_sized(void* mem, size_t n_bytes){
+    dealloc(mem);
+  };
+};
