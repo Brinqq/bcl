6fbf851cf6386313b2c95253969e371eb616e76d
brinq <andrewrdavis18@gmail.com>
2026-07-27 19:53:33 -0700

    defines, renaming, and aligned_alloc fix

--- src/include/bk/memory/bkmemory.h
+++ src/include/bk/memory/bkmemory.h
@@ -14,33 +14,45 @@ namespace bcl{
 }
 
 namespace bk{
-  typedef char byte;
+  typedef unsigned char byte;
 
-  _bkconstexpr11 size_t AlignP2(const size_t size, const size_t align){
+
+  _bkconstexpr11 size_t align_p2(const size_t size, const size_t align){
     return (size + align - 1) & ~(align - 1);
   }
 
-  _bkconstexpr11 size_t AlignedSize(const size_t size, const size_t align){
+  _bkconstexpr11 size_t aligned_size(const size_t size, const size_t align){
     return (size + align) - (size % align);
   }
 
-  _bkconstexpr11 bool IsPowerOfTwo(size_t val){
+  _bkconstexpr11 bool check_power2(size_t val){
     return (val != 0) && ((val & (val - 1)) == 0);
   };
 
-inline void* AlignedAlloc(size_t size, size_t align){
+  /**
+   * \brief Aligns a pointer to the next aligned address.
+   */
+  inline void* align_pointer(const void* ptr, size_t align){
+    uintptr_t ret = align_p2((uintptr_t)ptr, align);
+    return (void*)ret ;
+  };
+
+inline void* align_alloc(size_t size, size_t align){
   #ifdef _WIN32
     return _aligned_malloc(size, align);
   #else
-  return aligned_alloc(align, size);
+    void* mem = nullptr;
+    posix_memalign(&mem, align, size);
+    return mem;
   #endif
  }
 
-inline void AlignedFree(void* mem){
+inline void align_free(void* mem){
  #ifdef _WIN32
   _aligned_free(mem);
  #else
   free(mem);
  #endif
  }
+
 }
--- src/include/bk/memory/common.h
+++ src/include/bk/memory/common.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "type_traits"
+#include "bk/defines.h"
+
 
 namespace bk{
 /**
