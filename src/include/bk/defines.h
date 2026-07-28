218d534d0cb2539d042ae4ec016487d2f58dc43d
brinq <andrewrdavis18@gmail.com>
2026-07-27 19:55:17 -0700

    co

--- src/include/bk/defines.h
+++ src/include/bk/defines.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <stddef.h>
+#include <stdint.h>
 #include <assert.h>
 
 namespace bk{
 
 /**
- * \breif empty class.
+ * \brief empty class.
 *  \note the only reason we dont use std::monostate is because the minimum std version is 17.
 *  \code
-*   std::conditional_t(bool, SomeStruct, bcl::monostate);
+  *   std::conditional_t(bool, SomeStruct, bcl::monostate);
 *  \endcode
 */
 struct Monostate{};
